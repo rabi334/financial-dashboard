4f5151818be55215d4eca83a8e311eca298c3c10
rabira <rabiraasefa541@gmail.com>
2026-02-22 15:03:12 +0300

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Lusitana,Inter } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+export const inter = Inter({
+  subsets: ['latin'],
+});
